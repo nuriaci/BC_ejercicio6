472475bd5b0e703574ba27c02f2f41b29159d26f
nuriaci <nuria.ci@hotmail.es>
2024-10-18 11:58:06 +0200

    añadida function: proponer contrato

--- Ejercicio6/AlquilerPisos.sol
+++ Ejercicio6/AlquilerPisos.sol
@@ -18,6 +18,12 @@ contract AlquilerPisos {
     bool public renovacionAceptadaPorArrendatario;
     bool public renovacionAceptadaPorArrendador;
     bool public pagoPrimerMes = false;
+
+    //Propuesta nuevo contrato
+    uint public nuevaDuracionPropuesta;
+    uint public nuevoPrecioPropuesta;
+    bool public propuestaPendiente = false;
+    uint public plazoPropuesta;
     
     // Eventos
     event pagoRealizado(uint _precio, uint _fecha);
@@ -47,6 +53,7 @@ contract AlquilerPisos {
         fechaInicioContrato = block.timestamp;
         fechaVencimientoPago = block.timestamp + 30 days;
         contratoActivo = true;
+        pagoPrimerMes = true;
     }
 
     // Pago de alquiler
@@ -54,8 +61,9 @@ contract AlquilerPisos {
         require(block.timestamp <= fechaVencimientoPago, "Se ha excedido la fecha de pago.");
         
         uint256 cantidadPago;
-        if(fechaInicioContrato <= block.timestamp &&  block.timestamp <= fechaVencimientoPago){
-            cantidadPago = precioAlquiler + fianzaAlquiler;          
+        if(pagoPrimerMes){
+            cantidadPago = precioAlquiler + fianzaAlquiler;   
+            pagoPrimerMes = false;       
         }else{
             cantidadPago = precioAlquiler;
         }
@@ -77,28 +85,34 @@ contract AlquilerPisos {
 
         emit devolucionFianza(_fianzaIncumplimiento, block.timestamp);
     }
-    //renovar contrato
-    function renovarContrato() public soloArrendador{
 
-    }
-
-    function proponerContrato(uint _nuevaDuracionContrato,uint _nuevaFianzaAlquiler) public soloArrendador{
+    function proponerContrato(uint _nuevaDuracionContrato,uint _nuevoPrecioAlquiler) public soloArrendador{
         uint fechaFinContrato = fechaInicioContrato+duracionContrato;
-        //Comprobaciones
+        //Comprobaciones para proponer un nuevo contrato
         require((contratoActivo == true)&&((fechaFinContrato - block.timestamp) >= 30 * 86400), "Quedan menos de 30 dias hasta el fin del contrato");
         require((contratoActivo == false)&&(fechaFinContrato > block.timestamp), "El contrato ya finalizo");
+        require(propuestaPendiente == false, "Ya hay una propuesta de contrato pendiente");
        
-       //aceptar contrato
+        //nueva proposicion
+        nuevaDuracionPropuesta = _nuevaDuracionContrato;
+        nuevoPrecioPropuesta = _nuevoPrecioAlquiler;
+        propuestaPendiente = true;
+        plazoPropuesta = block.timestamp;
+        //se vuelve a pagar fianza??
 
-       //
-        
+        //aceptar contrato
+        emit propuestaRenovacion(_nuevaDuracionContrato, _nuevoPrecioAlquiler);     
         
     }
     function aceptarContrato() public soloArrendatario{
+        require(propuestaPendiente == true, "No hay ninguna propuesta de contrato");
+        require(plazoPropuesta + 30 days >= block.timestamp, "La propuesta ha finalizado");
 
+        duracionContrato = nuevaDuracionPropuesta;
+        precioAlquiler = nuevoPrecioPropuesta;
+        propuestaPendiente = false;
 
-
-        emit acuerdoAlcanzado(nuevaDuracion, nuevoPrecio);
+        emit acuerdoAlcanzado(duracionContrato, precioAlquiler);
     }
 
 
